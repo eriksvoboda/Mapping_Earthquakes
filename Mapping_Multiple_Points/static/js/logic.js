646b39ca320d9477bba254f51c63e2662d2ab9fa
Erik Svoboda <erik4svoboda7@gmail.com>
2022-03-20 10:13:56 -0500

    Adding Files

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -16,14 +16,20 @@ let map = L.map("mapid", {
 // add a marker to the map for LA 
 //let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-//add a circle to the map
 
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: 'black',
-    fillColor: '#ffffa1'
- }).addTo(map);
+// get data from cities.js
+let cityData = cities;
 
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: 'red'
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
 
 // We createe the tile Layer that will be the backgroud of our map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
